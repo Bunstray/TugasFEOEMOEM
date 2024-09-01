21494208cbf1a7106d0b329a262efde3af65559f
Bunstray <rayyanbunasatria06@gmail.com>
2024-09-01 19:36:23 +0700

    i forgor

--- wcss.js
+++ wcss.js
@@ -13,26 +13,20 @@ function melek() {
 }
 
 var counter = 1;
-var counter2 = 0;
 setInterval(function geser(){
   document.getElementById('radio' + counter).checked = true;
-  document.getElementById('radio' + counter2).checked = false;
   counter++;
-  counter2++;
   if(counter>3){
-    counter = 1;
-  }
-  if(counter2>3){
     counter = 1;
   }
 }, 5000);
 
 function t1(){
-  counter = 1;
-}
-function t2(){
   counter = 2;
 }
-function t3(){
+function t2(){
   counter = 3;
+}
+function t3(){
+  counter = 1;
 }
